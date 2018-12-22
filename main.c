02f295a78c436109763e6efccd346b5508751925
Karol Herbst <karolherbst@posteo.net>
2018-12-22 19:39:53 +0100

    remove unneeded includes

--- main.c
+++ main.c
@@ -8,14 +8,11 @@
 #define __USE_GNU 1
 #include <pthread.h>
 
-#include <arpa/inet.h>
-
 #include <event2/buffer.h>
 #include <event2/bufferevent.h>
 #include <event2/event.h>
 #include <event2/listener.h>
 #include <event2/thread.h>
-#include <fcntl.h>
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
