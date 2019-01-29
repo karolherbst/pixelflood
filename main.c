6089ea20259769f80483934cd5d4edfebc14694f
Karol Herbst <karolherbst@posteo.net>
2019-01-29 02:55:49 +0100

    more performant displaying with direct OpenGL usage

--- main.c
+++ main.c
@@ -21,7 +21,10 @@
 
 #include <fontconfig/fontconfig.h>
 
+#include <GL/glcorearb.h>
+
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
 #include <SDL2/SDL_ttf.h>
 
 #define likely(x)       __builtin_expect((x),1)
@@ -42,7 +45,7 @@ static _Atomic uint32_t nr_clients;
 static const uint32_t WIDTH = 1920;
 static const uint32_t HEIGHT = 1080;
 static const uint32_t THREADS = 8;
-static const char * IP = "151.217.200.48:12345";
+static const char IP[] = "151.217.200.48:12345";
 static const uint16_t PORT = 12345;
 static const uint8_t FONT_SIZE = 44;
 static const float FPS_INTERVAL = 1.0; //seconds
@@ -155,6 +158,54 @@ quit_application(void)
 		event_base_loopbreak(thread_data[i].evbase);
 }
 
+const char text_default[] = "FPS: XXXX Clients: XXXXX Mp: XXXXXXXX kp/s: XXXXXXX Mbit/s: XXXXXXX";
+const char text2_default[] = "IP:                      ";
+
+struct draw_state {
+	char *text;
+	char *text2;
+	TTF_Font *font;
+};
+
+static bool
+draw_state_init(struct draw_state *ds)
+{
+	ds->text = strdup(text_default);
+	ds->text2 = strdup(text2_default);
+
+	FcConfig* fc = FcInitLoadConfigAndFonts();
+	FcPattern* pat = FcNameParse((FcChar8 *)"FreeMono");
+	FcConfigSubstitute(fc, pat, FcMatchPattern);
+	FcDefaultSubstitute(pat);
+
+	FcResult result;
+	FcPattern* fcfont = FcFontMatch(fc, pat, &result);
+	FcChar8* file = NULL;
+	if (fcfont)
+		FcPatternGetString(fcfont, FC_FILE, 0, &file);
+	if (!file) {
+		printf("Couldn't find FreeMono.ttf\n");
+		return false;
+	}
+
+	ds->font = TTF_OpenFont((char *)file, FONT_SIZE);
+
+	FcPatternDestroy(fcfont);
+	FcPatternDestroy(pat);
+	FcConfigDestroy(fc);
+	FcFini();
+
+	return true;
+}
+
+static void
+draw_state_fini(struct draw_state *ds)
+{
+	TTF_CloseFont(ds->font);
+	free(ds->text);
+	free(ds->text2);
+}
+
 struct draw_funcs {
 	SDL_RendererFlags sdl_renderer_flags;
 };
@@ -170,17 +221,10 @@ struct draw_funcs draw_funcs_sw = {
 static int
 sdl_gl_draw_loop(SDL_Window *window, struct draw_funcs *funcs)
 {
-	pixels = malloc(sizeof(*pixels) * WIDTH * HEIGHT);
-	mtx_unlock(&px_mtx);
-
-	char text[] = "FPS: XXXX Clients: XXXXX Mp: XXXXXXXX kp/s: XXXXXXX Mbit/s: XXXXXXX";
-	char text2[] = "IP:                      ";
-
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, funcs->sdl_renderer_flags);
 	if (!renderer) {
 		printf("failed to create renderer\n");
-		exit(-1);
-		return -1;
+		return -2;
 	}
 
 	SDL_RenderClear(renderer);
@@ -199,45 +243,31 @@ sdl_gl_draw_loop(SDL_Window *window, struct draw_funcs *funcs)
 	}
 	if (!found_format) {
 		printf("couldn't find supported texture format\n");
-		exit(-1);
-		return -1;
+		SDL_DestroyRenderer(renderer);
+		return -2;
 	}
 
 	SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN_DESKTOP);
 	SDL_ShowCursor(SDL_DISABLE);
 
-	SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, WIDTH, HEIGHT);
-
-	FcConfig* fc = FcInitLoadConfigAndFonts();
-	FcPattern* pat = FcNameParse((FcChar8 *)"FreeMono");
-	FcConfigSubstitute(fc, pat, FcMatchPattern);
-	FcDefaultSubstitute(pat);
-
-	FcResult result;
-	FcPattern* fcfont = FcFontMatch(fc, pat, &result);
-	FcChar8* file = NULL;
-	if (fcfont)
-		FcPatternGetString(fcfont, FC_FILE, 0, &file);
-	if (!file) {
-		printf("Couldn't find FreeMono.ttf\n");
-		exit(-1);
+	struct draw_state ds;
+	if (!draw_state_init(&ds)) {
+		printf("failed to init draw state\n");
 		return -1;
 	}
 
-	TTF_Init();
-	TTF_Font *font = TTF_OpenFont((char *)file, FONT_SIZE);
+	pixels = malloc(sizeof(*pixels) * WIDTH * HEIGHT);
+	mtx_unlock(&px_mtx);
+
+	SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, WIDTH, HEIGHT);
+
 	SDL_Color tcolor = { 255, 255, 255 };
 	SDL_Color bgcolor = { 0, 0, 0 };
-	memcpy(&text2[4], IP, strlen(IP));
-	SDL_Surface *tsurface = TTF_RenderText_Shaded(font, "Please stand by!                                 ", tcolor, bgcolor);
-	SDL_Surface *t2surface = TTF_RenderText_Shaded(font, text2, tcolor, bgcolor);
+	memcpy(&ds.text2[4], IP, sizeof(IP));
+	SDL_Surface *tsurface = TTF_RenderText_Shaded(ds.font, "Please stand by!                                 ", tcolor, bgcolor);
+	SDL_Surface *t2surface = TTF_RenderText_Shaded(ds.font, ds.text2, tcolor, bgcolor);
 	SDL_Texture *ttexture = SDL_CreateTextureFromSurface(renderer, tsurface);
 	SDL_Texture *t2texture = SDL_CreateTextureFromSurface(renderer, t2surface);
-
-	FcPatternDestroy(fcfont);
-	FcPatternDestroy(pat);
-	FcConfigDestroy(fc);
-	FcFini();
 
 	SDL_Event event;
 	uint32_t fps_lasttime = SDL_GetTicks();
@@ -281,11 +311,11 @@ sdl_gl_draw_loop(SDL_Window *window, struct draw_funcs *funcs)
 			fps_current = fps_frames;
 			fps_frames = 0;
 
-			insert_nr_dec(&text[5], fps_current, 4);
-			insert_nr_dec(&text[19], threads, 5);
-			insert_nr_dec(&text[29], pixels / 1000000, 8);
-			insert_nr_dec(&text[44], (pixels - px_last) / 1000, 7);
-			insert_nr_dec(&text[60], data_cnt / 125000, 7);
+			insert_nr_dec(&ds.text[5], fps_current, 4);
+			insert_nr_dec(&ds.text[19], threads, 5);
+			insert_nr_dec(&ds.text[29], pixels / 1000000, 8);
+			insert_nr_dec(&ds.text[44], (pixels - px_last) / 1000, 7);
+			insert_nr_dec(&ds.text[60], data_cnt / 125000, 7);
 
 			SDL_Log("%f,%u,%"PRIu64",%"PRIu64",%"PRIu64"\n", fps, threads, pixels, pixels - px_last, data_cnt);
 
@@ -294,7 +324,7 @@ sdl_gl_draw_loop(SDL_Window *window, struct draw_funcs *funcs)
 
 			SDL_DestroyTexture(ttexture);
 			SDL_FreeSurface(tsurface);
-			tsurface = TTF_RenderText_Shaded(font, (const char*)&text, tcolor, bgcolor);
+			tsurface = TTF_RenderText_Shaded(ds.font, ds.text, tcolor, bgcolor);
 			ttexture = SDL_CreateTextureFromSurface(renderer, tsurface);
 		}
 
@@ -309,12 +339,14 @@ sdl_gl_draw_loop(SDL_Window *window, struct draw_funcs *funcs)
 	SDL_DestroyTexture(t2texture);
 	SDL_FreeSurface(tsurface);
 	SDL_FreeSurface(t2surface);
-	TTF_CloseFont(font);
-	TTF_Quit();
 
 	SDL_DestroyTexture(texture);
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
+
+	draw_state_fini(&ds);
+
+	TTF_Quit();
 	SDL_Quit();
 
 	mtx_lock(&px_mtx);
@@ -323,15 +355,371 @@ sdl_gl_draw_loop(SDL_Window *window, struct draw_funcs *funcs)
 	return 0;
 }
 
+const char* vs_src =
+"#version 150 core\n"
+"#extension GL_ARB_explicit_attrib_location : require\n"
+"layout (location = 0) in vec4 aPos;\n"
+"\n"
+"out vec2 coords;\n"
+"\n"
+"void main()\n"
+"{\n"
+"	gl_Position = vec4(aPos.x - 1.0, aPos.y - 1.0, 0.0, 1.0);\n"
+"	coords.x = aPos.x * aPos.z;\n"
+"	coords.y = 1.0 - (aPos.y * aPos.w);\n"
+"}\n";
+
+const char *fs_src =
+"#version 150 core\n"
+"out vec4 color;\n"
+"\n"
+"uniform sampler2D tex;"
+"\n"
+"in vec2 coords;\n"
+"\n"
+"void main()\n"
+"{\n"
+"	color = texture(tex, vec2(coords.x, coords.y));\n"
+"}\n";
+
+#define GL_API_PROC(name, def) \
+PFNGL##def##PROC name = SDL_GL_GetProcAddress(#name)
+
+#define check_compile_error(shader) \
+do { \
+	int success; \
+	char infoLog[512]; \
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success); \
+	if (!success) \
+	{ \
+		glGetShaderInfoLog(shader, 512, 0, infoLog); \
+		printf("%s\n", infoLog); \
+	} \
+} while (false)
+
+#define check_link_error(program) \
+do { \
+	int success; \
+	char infoLog[512]; \
+	glGetProgramiv(program, GL_LINK_STATUS, &success); \
+	if (!success) { \
+		glGetProgramInfoLog(program, 512, 0, infoLog); \
+		printf("%s\n", infoLog); \
+	} \
+} while (false)
+
+static int
+gl_draw_loop(SDL_Window *window, struct draw_funcs *funcs)
+{
+	GL_API_PROC(glAttachShader,			ATTACHSHADER);
+	GL_API_PROC(glBindBuffer,			BINDBUFFER);
+	GL_API_PROC(glBindTexture, 			BINDTEXTURE);
+	GL_API_PROC(glBindVertexArray,			BINDVERTEXARRAY);
+	GL_API_PROC(glBufferData,			BUFFERDATA);
+	GL_API_PROC(glBufferStorage,			BUFFERSTORAGE);
+	GL_API_PROC(glCompileShader,			COMPILESHADER);
+	GL_API_PROC(glCreateProgram,			CREATEPROGRAM);
+	GL_API_PROC(glCreateShader,			CREATESHADER);
+	GL_API_PROC(glDeleteBuffers,			DELETEBUFFERS);
+	GL_API_PROC(glDeleteShader,             	DELETESHADER);
+	GL_API_PROC(glDeleteVertexArrays,		DELETEVERTEXARRAYS);
+	GL_API_PROC(glDrawElements,			DRAWELEMENTS);
+	GL_API_PROC(glEnableVertexAttribArray,		ENABLEVERTEXATTRIBARRAY);
+	GL_API_PROC(glFlushMappedBufferRange,		FLUSHMAPPEDBUFFERRANGE);
+	GL_API_PROC(glGenBuffers,			GENBUFFERS);
+	GL_API_PROC(glGenTextures, 			GENTEXTURES);
+	GL_API_PROC(glGenVertexArrays,			GENVERTEXARRAYS);
+	GL_API_PROC(glGetProgramiv,			GETPROGRAMIV);
+	GL_API_PROC(glGetProgramInfoLog,		GETPROGRAMINFOLOG);
+	GL_API_PROC(glGetShaderiv,			GETSHADERIV);
+	GL_API_PROC(glGetShaderInfoLog,			GETSHADERINFOLOG);
+	GL_API_PROC(glGetUniformLocation,		GETUNIFORMLOCATION);
+	GL_API_PROC(glLinkProgram,			LINKPROGRAM);
+	GL_API_PROC(glMapBuffer,			MAPBUFFER);
+	GL_API_PROC(glMapBufferRange,			MAPBUFFERRANGE);
+	GL_API_PROC(glShaderSource,			SHADERSOURCE);
+	GL_API_PROC(glTexImage2D, 			TEXIMAGE2D);
+	GL_API_PROC(glTexParameteri, 			TEXPARAMETERI);
+	GL_API_PROC(glTexSubImage2D,			TEXSUBIMAGE2D);
+	GL_API_PROC(glUniform1i,			UNIFORM1I);
+	GL_API_PROC(glUnmapBuffer,			UNMAPBUFFER);
+	GL_API_PROC(glUseProgram,			USEPROGRAM);
+	GL_API_PROC(glVertexAttribPointer,		VERTEXATTRIBPOINTER);
+	GL_API_PROC(glViewport,				VIEWPORT);
+
+	SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN_DESKTOP);
+	SDL_ShowCursor(SDL_DISABLE);
+
+	struct draw_state ds;
+	if (!draw_state_init(&ds)) {
+		printf("failed to init draw state\n");
+		return -1;
+	}
+
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+	SDL_GLContext glcontext = SDL_GL_CreateContext(window);
+
+	if (!glcontext) {
+		SDL_GL_ResetAttributes();
+		printf("failed to create GL context: %s\n", SDL_GetError());
+		return -2;
+	}
+
+	bool use_gpu_buffer = true;
+	if (!SDL_GL_ExtensionSupported("GL_ARB_buffer_storage")) {
+		use_gpu_buffer = false;
+		printf("GL_ARB_buffer_storage not supported, zero-CPU-overhead rendering not possible\n");
+	}
+
+	SDL_Color tcolor = { 255, 255, 255, 0 };
+	SDL_Color bgcolor = { 0, 0, 0, 0 };
+	memcpy(&ds.text2[4], IP, sizeof(IP));
+	SDL_Surface *tsurface = TTF_RenderText_Shaded(ds.font, "Please stand by!                                 ", tcolor, bgcolor);
+	SDL_Surface *t2surface = TTF_RenderText_Shaded(ds.font, ds.text2, tcolor, bgcolor);
+
+	SDL_Event event;
+	uint32_t fps_lasttime = SDL_GetTicks();
+	uint32_t fps_current;
+	uint32_t fps_frames = 0;
+
+	uint64_t px_last = 0;
+
+	int w, h;
+	SDL_GetWindowSize(window, &w, &h);
+	glViewport(0, 0, w, h);
+
+	float vertices[] = {
+		 2.0f,  2.0f, 0.5f, 0.5f,
+		 2.0f,  0.0f, 0.5f, 0.5f,
+
+		 0.0f,  0.0f, 0.5f, 0.5f,
+		 0.0f,  2.0f, 0.5f, 0.5f,
+
+		 2.0f,  0.125f, 0.5f, 8.0f,
+		 2.0f,  0.0f,   0.5f, 8.0f,
+
+		 0.0f,  0.0f,   0.5f, 8.0f,
+		 0.0f,  0.125f, 0.5f, 8.0f,
+	};
+	unsigned int indicesp[] = {
+		0, 1, 3,
+		1, 2, 3,
+	};
+	unsigned int indicest[] = {
+		4, 5, 7,
+		5, 6, 7,
+	};
+
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs, 1, &vs_src, NULL);
+	glCompileShader(vs);
+	check_compile_error(vs);
+
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs, 1, &fs_src, NULL);
+	glCompileShader(fs);
+	check_compile_error(fs);
+
+	GLuint program = glCreateProgram();
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	check_link_error(program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	GLuint VBO, VAO, PBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &PBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// with buffer_storage we can just map GPU memory and never synchronize
+	glBindBuffer(GL_PIXEL_UNPACK_BUFFER, PBO);
+	if (use_gpu_buffer) {
+		GLbitfield flags = GL_MAP_READ_BIT | GL_MAP_WRITE_BIT | GL_MAP_PERSISTENT_BIT;
+		glBufferStorage(GL_PIXEL_UNPACK_BUFFER, WIDTH * HEIGHT * 4, NULL, flags);
+		pixels = glMapBufferRange(GL_PIXEL_UNPACK_BUFFER, 0, WIDTH * HEIGHT * 4, flags | GL_MAP_FLUSH_EXPLICIT_BIT);
+		mtx_unlock(&px_mtx);
+	} else {
+		glBufferData(GL_PIXEL_UNPACK_BUFFER, WIDTH * HEIGHT * 4, NULL, GL_STREAM_DRAW);
+		pixels = malloc(sizeof(*pixels) * WIDTH * HEIGHT);
+		mtx_unlock(&px_mtx);
+	}
+	glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
+
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	GLuint ptexture;
+	glGenTextures(1, &ptexture);
+	glBindTexture(GL_TEXTURE_2D, ptexture);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	glBindBuffer(GL_PIXEL_UNPACK_BUFFER, PBO);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, WIDTH, HEIGHT, 0, GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, NULL);
+	glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
+
+	GLuint ttexture;
+	glGenTextures(1, &ttexture);
+	glBindTexture(GL_TEXTURE_2D, ttexture);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	SDL_PixelFormat *dst_fmt = SDL_AllocFormat(SDL_PIXELFORMAT_ARGB8888);
+	SDL_Surface *temp1 = SDL_ConvertSurface(tsurface, dst_fmt, 0);
+	SDL_Surface *temp2 = SDL_ConvertSurface(t2surface, dst_fmt, 0);
+	SDL_FreeFormat(dst_fmt);
+
+	GLuint ccolor[WIDTH * HEIGHT];
+	memset(&ccolor, 0, WIDTH * HEIGHT);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, WIDTH, temp1->h + temp2->h, 0, GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, &ccolor);
+	glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, temp1->w, temp1->h, GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, temp1->pixels);
+	glTexSubImage2D(GL_TEXTURE_2D, 0, 0, temp1->h, temp2->w, temp2->h, GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, temp2->pixels);
+	SDL_FreeSurface(temp1);
+
+	glUseProgram(program);
+	glUniform1i(glGetUniformLocation(program, "tex"), 0);
+
+	SDL_Log("fps,clients,pixels,pixels_per_second,data_kb\n");
+	bool running = true;
+	while (running) {
+		if (SDL_PollEvent(&event)) {
+			switch (event.type) {
+			case SDL_KEYDOWN: {
+				switch (event.key.keysym.sym) {
+				case SDLK_q:
+					quit_application();
+					running = false;
+				}
+				break;
+			}
+			case SDL_QUIT:
+				quit_application();
+				running = false;
+				break;
+			}
+		}
+
+		fps_frames++;
+		if (fps_lasttime < (SDL_GetTicks() - FPS_INTERVAL * 1000))
+		{
+			uint64_t pixels = nr_pixels;
+			uint32_t threads = nr_clients;
+			float fps = ((float)1000 * fps_frames) / (SDL_GetTicks() - fps_lasttime);
+			fps_lasttime = SDL_GetTicks();
+			fps_current = fps_frames;
+			fps_frames = 0;
+
+			insert_nr_dec(&ds.text[5], fps_current, 4);
+			insert_nr_dec(&ds.text[19], threads, 5);
+			insert_nr_dec(&ds.text[29], pixels / 1000000, 8);
+			insert_nr_dec(&ds.text[44], (pixels - px_last) / 1000, 7);
+			insert_nr_dec(&ds.text[60], data_cnt / 125000, 7);
+
+			SDL_Log("%f,%u,%"PRIu64",%"PRIu64",%"PRIx64"\n", fps, threads, pixels, pixels - px_last, data_cnt);
+
+			px_last = pixels;
+			data_cnt = 0;
+
+			SDL_FreeSurface(tsurface);
+			tsurface = TTF_RenderText_Shaded(ds.font, ds.text, tcolor, bgcolor);
+
+			SDL_PixelFormat *dst_fmt = SDL_AllocFormat(SDL_PIXELFORMAT_ARGB8888);
+			temp1 = SDL_ConvertSurface(tsurface, dst_fmt, 0);
+			SDL_FreeFormat(dst_fmt);
+
+			glBindTexture(GL_TEXTURE_2D, ttexture);
+			glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, temp1->w, temp1->h, GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, temp1->pixels);
+			glTexSubImage2D(GL_TEXTURE_2D, 0, 0, temp1->h, temp2->w, temp2->h, GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, temp2->pixels);
+
+			SDL_FreeSurface(temp1);
+		}
+
+		glBindTexture(GL_TEXTURE_2D, ptexture);
+		glBindBuffer(GL_PIXEL_UNPACK_BUFFER, PBO);
+		// without buffer_storage we have to upload data to the PBO
+		if (!use_gpu_buffer) {
+			void *pboPixels = glMapBuffer(GL_PIXEL_UNPACK_BUFFER, GL_WRITE_ONLY);
+			memcpy(pboPixels, pixels, WIDTH * HEIGHT * 4);
+			glUnmapBuffer(GL_PIXEL_UNPACK_BUFFER);
+		} else {
+			glFlushMappedBufferRange(GL_PIXEL_UNPACK_BUFFER, 0, WIDTH * HEIGHT * 4);
+		}
+		glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, WIDTH, HEIGHT, GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, NULL);
+		glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, indicesp);
+
+		glBindTexture(GL_TEXTURE_2D, ttexture);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, indicest);
+
+		SDL_GL_SwapWindow(window);
+	}
+
+	glBindBuffer(GL_PIXEL_UNPACK_BUFFER, PBO);
+	mtx_lock(&px_mtx);
+	if (use_gpu_buffer)
+		glUnmapBuffer(GL_PIXEL_UNPACK_BUFFER);
+	else
+		free(pixels);
+	mtx_unlock(&px_mtx);
+	glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &PBO);
+
+	SDL_FreeSurface(temp2);
+	SDL_FreeSurface(tsurface);
+	SDL_FreeSurface(t2surface);
+
+	SDL_GL_DeleteContext(glcontext);
+	SDL_DestroyWindow(window);
+
+	draw_state_fini(&ds);
+
+	TTF_Quit();
+	SDL_Quit();
+
+	return 0;
+}
+
 static int
 sdl_draw_loop(void *ptr)
 {
 	SDL_Window *window;
 	SDL_InitSubSystem(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
+	TTF_Init();
 	window = SDL_CreateWindow("pixelflood", 0, 0, WIDTH, HEIGHT,
 		SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_BORDERLESS | SDL_WINDOW_RESIZABLE);
-	if (window)
-		return sdl_gl_draw_loop(window, &draw_funcs_gl);
+	if (window) {
+		int err = gl_draw_loop(window, &draw_funcs_gl);
+		if (err != -2)
+			return err;
+		printf("failed to create OpenGL context. Falling back to hw SDL rendering\n");
+
+		err = sdl_gl_draw_loop(window, &draw_funcs_gl);
+		if (err != -2)
+			return err;
+
+		printf("failed to create hw SDL context. Falling back to sw SDL rendering\n");
+		SDL_DestroyWindow(window);
+	}
 
 	window = SDL_CreateWindow("pixelflood", 0, 0, WIDTH, HEIGHT,
 		SDL_WINDOW_SHOWN | SDL_WINDOW_BORDERLESS | SDL_WINDOW_RESIZABLE);
