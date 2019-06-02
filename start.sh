dbdd73e79db3a423102f00f0f09ba559c3672457
Karol Herbst <karolherbst@posteo.net>
2019-06-02 21:44:03 +0200

    start.sh: build libevent
    should lower network code overhead by making use of lto and more aggressive
    optimizations

--- start.sh
+++ start.sh
@@ -1,12 +1,44 @@
 #!/bin/sh
-if [ ! -d "build" ]; then
-	mkdir build
-	cd build
-	cmake .. -DCMAKE_BUILD_TYPE=Release
-	cd ..
+git submodule init
+git submodule update
+if [ ! -d "build/libevent" ]; then
+	mkdir -p build/libevent
+	cd build/libevent
+	cmake ../../libevent \
+		-DCMAKE_BUILD_TYPE=Release \
+		-DEVENT__LIBRARY_TYPE=STATIC \
+		-DEVENT__DISABLE_DEBUG_MODE=ON \
+		-DEVENT__ENABLE_VERBOSE_DEBUG=OFF \
+		-DEVENT__DISABLE_THREAD_SUPPORT=OFF \
+		-DEVENT__DISABLE_OPENSSL=ON \
+		-DEVENT__DISABLE_BENCHMARK=ON \
+		-DEVENT__DISABLE_TESTS=ON \
+		-DEVENT__DISABLE_REGRESS=ON \
+		-DEVENT__DISABLE_SAMPLES=ON \
+		-DEVENT__DISABLE_CLOCK_GETTIME=OFF \
+		-DEVENT__DISABLE_GCC_WARNINGS=ON \
+		-DEVENT__ENABLE_GCC_HARDENING=ON \
+		-DEVENT__ENABLE_GCC_WARNINGS=OFF \
+		-DCMAKE_SKIP_RPATH=YES \
+		-DCMAKE_C_FLAGS="-Ofast -mtune=native -march=native -flto" \
+		-DCMAKE_AR="$(whereis -b gcc-ar | cut -d\  -f2)" \
+		-DCMAKE_RANLIB="$(whereis -b gcc-ranlib | cut -d\  -f2)" \
+		-DCMAKE_INSTALL_PREFIX="../local"
+	cd ../..
 fi
 
-cmake --build build
+cmake --build build/libevent
+cmake --build build/libevent --target install
+export PKG_CONFIG_PATH=$PWD/build/local/lib/pkgconfig/
+
+if [ ! -d "build/pixelflood" ]; then
+	mkdir -p build/pixelflood
+	cd build/pixelflood
+	cmake ../.. -DCMAKE_BUILD_TYPE=Release
+	cd ../..
+fi
+
+cmake --build build/pixelflood
 export MESA_NO_ERROR=1
 #export SDL_VIDEODRIVER=kmsdrm
 #/usr/bin/valgrind --tool=drd \
@@ -15,4 +47,4 @@ export MESA_NO_ERROR=1
 #/usr/bin/valgrind --tool=cachegrind \
 #perf stat -a \
 #perf record --call-graph=dwarf \
-build/pixelflood
+build/pixelflood/pixelflood
